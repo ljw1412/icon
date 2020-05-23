242b995623e3b9e14928f8c7014fdb243452943f
ljw1412 <506644791@qq.com>
2020-05-23 13:36:11 +0800

    chore: 兼容性语法

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-const svgs = require.context('./svg', false, /\.svg$/)
-const requireAll = requireContext => requireContext.keys().map(requireContext)
+var svgs = require.context('./svg', false, /\.svg$/)
+var requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(svgs)
